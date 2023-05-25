55b3311b21a748e69141a0dea49ef1150e24eb10
Abhay kumar srivastava <120777323+srivastavm089@users.noreply.github.com>
2023-05-25 13:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Edit this file */
+
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -6,3 +6,23 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+
+toggle.addEventListener('click', ()=>{
+	if(video.paused){
+		video.play();
+		toggle.textContent ="❚ ❚"
+	}else{
+		video.pause();
+		toggle.textContent = "►"
+	}
+});
+
+const progressBar = document.getElementById("progressBar");
+
+video.addEventListener("timeupdate", function() {
+  const progress = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = progress
+});
+
+
